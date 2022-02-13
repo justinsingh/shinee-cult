4d00067fba978203b3798fbf64bd87d3c000e0ce
Justin Singh <justinsingh2015@gmail.com>
2022-02-13 14:03:55 -0800

    Use cropped banner image and styling improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
 import type { NextPage } from 'next'
 //import Head from 'next/head'
 import { Box, Center, Image, Text, VStack, Heading } from '@chakra-ui/react'
-import Banner from '../public/shinee-cult-banner.png'
+import Banner from '../public/cropped-shinee-cult-banner.png'
 
 
 const Home: NextPage = () => {
   return (
     <Box>
-      <VStack align="left" spacing={["-7%"]}>
-        <Text color="white" fontSize={["7.5vw", "4.6vw"]} mt={[20]} ml={["10%"]}>
+      <VStack align="left">
+        <Text color="white" fontSize={["7.5vw", "4.6vw"]} mt={["15%", "5%"]} ml={["10%"]}>
           Welcome To . . .
         </Text>
-        <Image width="100%" height="100%" src={Banner.src} alt="Shinee Cult Banner" />
+        <Center>
+          <Image mt={[0, 5]} width="80%" height="80%" src={Banner.src} alt="Shinee Cult Banner" />
+        </Center>
       </VStack>
     </Box>
   )
